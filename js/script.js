7b3223d5e304f96001b97c973fc9b8e1ed4af6aa
V A <V A>
2016-08-11 10:46:07 +0300

    entry page fixed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(function() {
+  // draw Directories page
   $.getJSON('json/getDirectories.json', function(data) {
     $.each(data.response.directories, function(i, item) {
       var list = "<li class='nav__item'>" + "<a href='#' class='link' id='" + item.id + "'>" + "<span class='underline'>" + "<span>" + item.name + "</span>" + "</span>" + "</a>" + "</li>"
@@ -6,6 +7,7 @@ $(function() {
     });
   });
 
+// draw Directory page
   $.getJSON('json/getDirectory.json', function(data) {
     $.each(data.response.directory.fields, function(i, item) {
       var rowHead = "<th>" + item.name + "</th>"
@@ -25,12 +27,15 @@ $(function() {
     });
   });
 
+// draw Entry page
   $.getJSON('json/getEntry.json', function(data) {
     $.each(data.response.entry.items, function(i, item) {
       var disabled = item.id === 'name' ? 'disabled' : '';
       var field = item.type === 'TEXTAREA' ? "<textarea rows=4 id=" + item.id + ">" + item.value + "</textarea>" : item.type === 'SELECT' ? "<select id='" + item.id + "'></select>" : "<input value='" + item.value + "' type='text'  id= '" + item.id + "'" + disabled + "/>"
       var tblBody = "<div class='row'>" + "<label class='entry-title'>" + item.name + "</label>" + field + "</div>"
       $(tblBody).appendTo("#entry-table");
+
+// draw Select box
       if (item.values !== undefined) {
         var options = '';
         for (var i = 0; i < item.values.length; i++) {
@@ -38,7 +43,10 @@ $(function() {
           options += "<option value='" + item.values[i].id + "'" + selected + ">" + item.values[i].name + "</option>";
         }
         $("#units").html(options);
+        $("#units").parent('.row').addClass('selectBox');
       }
+
+// datepicker iptions
       $('#endDate').datepicker({
         showOn: "button",
         buttonImage: "img/date.png",
